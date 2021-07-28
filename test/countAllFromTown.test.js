ffe54a15c8634aeb79d278c0f99750b7f25e75a7
sp010412 <smajwede7@gmail.com>
2021-07-28 22:10:55 +0200

    complete

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -6,6 +6,6 @@ describe('countAllFromTown', function () {
         assert.deepEqual(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'), 3)
     });
     it('should return total number of registration plates, for plates that start with CL', function () {
-        assert.deepEqual(countAllFromTown('CY 567,CL 345, CJ 456,CL 124,CY 567,CJ 456','CL'), 3)
+        assert.deepEqual(countAllFromTown('CY 567,CL 345, CJ 456,CL 124,CY 567,CJ 456','CL'), 2)
     });
 });
